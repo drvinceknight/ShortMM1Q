8582039930c22f3397a57419fe0cf0cba73b8778
vince <vincent.knight@gmail.com>
2014-11-11 19:18:17 +0000

    Adding screenshot

--- MM1Q.py
+++ MM1Q.py
@@ -1,13 +1,12 @@
-# Shortest possible simulation of an MM1 Q
+# Shortest possible simulation of an MM1 Q (with warmup). Contributors: Geraint, Jason and Vince
 from random import expovariate
 lmbda, mu, T, warmup, waits, sample = 1, 2, 5000, 1000, [], lambda x: expovariate(x)
 arrival_time = sample(lmbda)
 start_time, end_time = arrival_time, arrival_time + sample(mu)
-
 while end_time < T:
     arrival_time += sample(lmbda)
     start_time = max(end_time, arrival_time)
     end_time = start_time + sample(mu)
     if arrival_time > warmup:
         waits.append(start_time - arrival_time)
-print sum(waits) / len(waits)
+print 'Mean wait: ' + str(sum(waits) / len(waits))
--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Trying to simulate an MM1Q in as little lines of code as possible
 
+We've got it down to 12 lines which includes a 'docstring' at the top, a warmup period and an output of a mean wait (so if we can get it any shorter please try and keep that functionality).
+A pull request would be awesome.
+
+Here's a screenshot of the output showing a mean wait of .5 which is the theoretical value for an arrival rate of 2 and a service rate of 1:
+
+![](screengrab.png)
+
 Contributors:
 
 - [Jason Young](https://github.com/JasYoung314)
