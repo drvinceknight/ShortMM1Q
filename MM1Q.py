0ec3f1c3ae40717b5ee0b0745d48603eb04f9e12
vince <vincent.knight@gmail.com>
2014-11-12 10:09:22 +0000

    Removing one line by having all variable assignments in same place

--- MM1Q.py
+++ MM1Q.py
@@ -1,11 +1,11 @@
 # Shortest possible simulation of an MM1 Q (with warmup). Contributors: Geraint, Jason and Vince
 from random import expovariate
-lmbda, mu, T, warmup, waits, sample = 1, 2, 5000, 1000, [], lambda x: expovariate(x)
-arrival_time = start_time = end_time = 0
+lmbda, mu, T, warmup, waits, sample, arrival_time, start_time, end_time = 1, 2, 5000, 1000, [], lambda x: expovariate(x), 0, 0, 0
 while end_time < T:
     arrival_time += sample(lmbda)
     start_time = max(end_time, arrival_time)
     end_time = start_time + sample(mu)
     if arrival_time > warmup:
         waits.append(start_time - arrival_time)
+
 print 'Mean wait: ' + str(sum(waits) / len(waits))
